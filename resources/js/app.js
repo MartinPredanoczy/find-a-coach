c6f6e07266c81aaeb127dc0720fed61097a02dd9
MartinPredanoczy <martin.predanoczy@gmail.com>
2022-06-21 13:21:50 +0200

    vytvorenie projektu

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+/*
+|--------------------------------------------------------------------------
+| Laravel Breeze
+|--------------------------------------------------------------------------
+| Alpine vyuziva Laravel Breeze
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+*/
+
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+ pre pripad implementacie autentifikacie
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes for Vue.js 3 integration
+|--------------------------------------------------------------------------
+|
+| routy pre vyuzitie Vue v Laravel aplikacii
+|
+*/
+
+Route::view('/', 'dashboard')->name('dashboard');

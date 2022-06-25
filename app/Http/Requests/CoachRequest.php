3a12fd6dc4697ea67979f84635d7f65e27dde683
MartinPredanoczy <martin.predanoczy@gmail.com>
2022-06-25 22:24:58 +0200

    vytvorenie validacneho form request suboru CoachRequest

---
+++ app/Http/Requests/CoachRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CoachRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'firstName' => ['required', 'string'],
+            'lastName' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'rate' => ['numeric|min:0', 'numeric|max:6']
+        ];
+    }
+}
